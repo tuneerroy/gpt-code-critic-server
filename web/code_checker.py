a5edb816bd62eafa396b2fec45f43996e7f02abb
Tuneer Roy <53184517+tuneerroy@users.noreply.github.com>
2023-05-26 12:24:25 -0400

    feat: entire application from prev repo

---
+++ web/code_checker.py
@@ -0,0 +1,194 @@
+import json
+import os
+
+import openai
+
+
+def analyze_file(filename, code_diff):
+    """
+    Analyze a file using GPT-3
+
+    Args:
+        filename: the name of the file to analyze
+        code_diff: the diff of the file to analyze
+
+    Returns:
+        result: the analysis of the file
+    """
+    # preface with explanation
+    prompt = "Analyze the following code changes to file " + filename + ".\n"
+    prompt += "You should format your response like:\n"
+    prompt += "Issue X (Line Y, Column Z)::: Comment about problem, quality, readability, or issue\n"
+    prompt += "Issue X (Line Y, Column Z)::: Comment about problem, quality, readability, or issue\n"
+    prompt += "...\n\n\n"
+    prompt += "diff:\n" + code_diff + "\n"
+
+    # send prompt to GPT-3
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo", messages=[{"role": "user", "content": prompt}]
+    )
+
+    # extract the generated analysis from the API response
+    result = completion.choices[0].message.content
+
+    return result
+
+
+def get_sarif_results_for_file(filename, analysis):
+    """
+    Get the SARIF results for a file
+
+    Args:
+        filename: the name of the file to analyze
+        analysis: the analysis of the file to analyze
+
+    Returns:
+        sarif_results: the SARIF results for the file
+    """
+    # parse the generated analysis from GPT-3
+    results = analysis.split("Issue")
+    issues = []
+    for result in results:
+        issue = {}
+        tokens = result.strip().split(":::")
+        if len(tokens) >= 2:
+            try:
+                issue["message"] = tokens[1].strip()
+                location = {}
+                location["file"] = filename
+                location["line"] = int(tokens[0].split("Line")[1].split(",")[0].strip())
+                location["column"] = int(tokens[0].split("Column")[1].strip()[:-1])
+                issue["location"] = location
+                issues.append(issue)
+            except:
+                pass
+
+    # create SARIF result objects
+    sarif_results = []
+    for issue in issues:
+        # extract issue details from the analysis
+        message = issue.get("message", "")
+        location = issue.get("location", {})
+        file_path = location.get("file", "")
+        line_number = location.get("line", 0)
+        column_number = location.get("column", 0)
+
+        # create SARIF result object
+        sarif_result = {
+            "message": {"text": message},
+            "locations": [
+                {
+                    "physicalLocation": {
+                        "artifactLocation": {"uri": file_path},
+                        "region": {
+                            "startLine": line_number,
+                            "startColumn": column_number,
+                        },
+                    }
+                }
+            ],
+        }
+
+        # add the SARIF result object to the list
+        sarif_results.append(sarif_result)
+
+    return sarif_results
+
+
+def combine_sarif_results(results):
+    """
+    Combine SARIF results into one report
+
+    Args:
+        results: the SARIF results to combine
+
+    Returns:
+        sarif_report: the combined SARIF report
+    """
+    sarif_results = sum(results, [])  # concatenate results into one list
+
+    # create SARIF report
+    sarif_report = {
+        "version": "2.1.0",
+        "runs": [
+            {
+                "tool": {
+                    "driver": {
+                        "name": "GPT-3.5 Code Analysis",
+                        "informationUri": "https://platform.openai.com/docs/models/overview",
+                        "version": "1.0",
+                    }
+                },
+                "results": sarif_results,
+            }
+        ],
+    }
+
+    return sarif_report
+
+
+def get_sarif_report(files, key=os.environ.get("OPENAI_API_KEY")):
+    """
+    Get the SARIF report for a list of files
+
+    Args:
+        files: the files to analyze
+        key: the API key to use (optional); defaults to OPENAI_API_KEY env variable
+
+    Returns:
+        sarif_report: the SARIF report for the files
+    """
+    # set API key
+    openai.api_key = key
+
+    # get SARIF results for each file
+    sarif_files = []
+    for name, code in files.items():
+        problems = analyze_file(name, code)
+        sarif_file = get_sarif_results_for_file(name, problems)
+        sarif_files.append(sarif_file)
+
+    # combine SARIF results into one report
+    sarif_report = combine_sarif_results(sarif_files)
+
+    # reset API key so it doesn't get saved
+    openai.api_key = None
+
+    return sarif_report
+
+
+def check_api_key(api_key):
+    """
+    Check if an API key is valid
+
+    Args:
+        api_key: the API key to check
+
+    Returns:
+        True if the API key is valid, False otherwise
+    """
+    openai.api_key = api_key
+    try:
+        response = openai.Completion.create(
+            engine="text-ada-001", prompt="Hello, world!", max_tokens=1
+        )
+        openai.api_key = None
+        if response.choices[0].text:
+            return True
+    except Exception as e:
+        print(e)
+    openai.api_key = None
+    return False
+
+
+"""
+Main function for testing
+"""
+if __name__ == "__main__":
+    filename = "main.py"
+    code = 'def foo():\n    prnt("Hello, world!")'
+
+    if os.environ.get("OPENAI_API_KEY") is None:
+        print("Please set OPENAI_API_KEY environment variable.")
+    else:
+        print(json.dumps(get_sarif_report({filename: code}), indent=2))
